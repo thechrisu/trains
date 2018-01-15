82f05466abeb0382824975b6bed12875e3ff0ce5
Thomas Broadley <buriedunderbooks@hotmail.com>
2018-01-15 08:21:19 -0500

    Implement scheduler

--- src/scheduler/scheduler.c
+++ src/scheduler/scheduler.c
@@ -1,18 +1,30 @@
 #include "scheduler.h"
 
 int scheduler_init(scheduler *s, int max_priority, ready_queue *queues) {
-  (void)s;
-  (void)max_priority;
-  (void)queues;
+  if (max_priority < 0)
+    return -1;
+
+  s->max_priority = max_priority;
+  s->queues = queues;
+  for (int i = 0; i <= max_priority; i += 1)
+    queues[i] = NULL_READY_QUEUE;
+
+  return 0;
 }
 
 int scheduler_register(scheduler *s, task_descriptor *td) {
-  (void)s;
-  (void)td;
+  int priority = td->priority;
+  if (priority < 0 || priority > s->max_priority)
+    return -1;
+  ready_queue_push(&(s->queues[priority]), td);
   return 0;
 }
 
 task_descriptor *scheduler_next_task(scheduler *s) {
-  (void)s;
-  return (task_descriptor *)0;
+  for (int i = s->max_priority; i >= 0; i -= 1) {
+    ready_queue *rq = &(s->queues[i]);
+    if (ready_queue_length(rq) > 0)
+      return ready_queue_pop(rq);
+  }
+  return NULL_TASK_DESCRIPTOR;
 }
