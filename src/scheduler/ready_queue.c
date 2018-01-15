64705f4ff592cf6f528a4e01d6af61851ef54ced
Thomas Broadley <buriedunderbooks@hotmail.com>
2018-01-15 06:58:23 -0500

    Implement ready_queue methods

--- src/scheduler/ready_queue.c
+++ src/scheduler/ready_queue.c
@@ -1,16 +1,52 @@
 #include "ready_queue.h"
 
 int ready_queue_length(ready_queue *rq) {
-  (void)rq;
-  return 0;
+  if (*rq == (ready_queue)0)
+    return 0;
+
+  int length = 0;
+  task_descriptor *td = *rq;
+  do {
+    td = td->next;
+    length += 1;
+  } while(td != *rq);
+  return length;
 }
 
 void ready_queue_push(ready_queue *rq, task_descriptor *td) {
-  (void)rq;
-  (void)td;
+  task_descriptor *head, *tail;
+
+  if (ready_queue_length(rq) == 0) {
+    td->prev = td;
+    td->next = td;
+    *rq = td;
+  } else {
+    head = *rq;
+    tail = head->prev;
+    head->prev = td;
+    tail->next = td;
+    td->prev = tail;
+    td->next = head;
+  }
 }
 
 task_descriptor *ready_queue_pop(ready_queue *rq) {
-  (void)rq;
-  return 0;
+  task_descriptor *head, *new_head, *tail;
+
+  switch (ready_queue_length(rq)) {
+    case 0:
+      return (task_descriptor *)0;
+    case 1:
+      head = *rq;
+      *rq = (ready_queue)0;
+      return head;
+    default:
+      head = *rq;
+      new_head = head->next;
+      tail = head->prev;
+      new_head->prev = tail;
+      tail->next = new_head;
+      *rq = new_head;
+      return head;
+  }
 }
