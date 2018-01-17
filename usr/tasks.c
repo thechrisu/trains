1065cce25273c23e3155b840afbb48649195852c
culshoefer <christophsulshoefer@gmail.com>
2018-01-16 22:08:14 -0500

    ADDED: User level functions as per spec for k1

--- usr/tasks.c
+++ usr/tasks.c
@@ -1,28 +1,24 @@
 #include "../include/common/codes.h"
 #include "myio.h"
 
-int first_user_task() {
-  int i;
-
-  bwprintf("We made it!\n\r");
+void other_user_task() {
+  bwprintf("In other task: MyTid(): %d, MyParentTid(): %d\n\r", MyTid(), MyParentTid());
   Pass();
-  bwprintf("We made it, twice\n\r");
-  bwprintf("MyTid: %x\n\r", MyTid());
-  Pass();
-  bwprintf("We made it, thrice\n\r");
-  Pass();
-  bwprintf("We made it, 4x\n\r");
-  Pass();
-  bwprintf("TASK 1: THIS SHOULD PRINT\n\r");
-  return 0xDEADBEEF;
+  bwprintf("In other task: MyTid(): %d, MyParentTid(): %d\n\r", MyTid(), MyParentTid());
+  Exit();
 }
 
-void second_user_task() {
-  bwprintf("Second user task once\n\r");
-  Pass();
-  bwprintf("Second user task twice\n\r");
-  Pass();
-  bwprintf("Second user task thrice\n\r");
-  Pass();
-  bwprintf("Second user task, finally, now return to main\n\r");
+int first_user_task() {
+  int new_task_id;
+  new_task_id = Create(4, &other_user_task);
+  bwprintf("Created: %d.\n\r", new_task_id);
+  new_task_id = Create(4, &other_user_task);
+  bwprintf("Created: %d.\n\r", new_task_id);
+  new_task_id = Create(6, &other_user_task);
+  bwprintf("Created: %d.\n\r", new_task_id);
+  new_task_id = Create(6, &other_user_task);
+  bwprintf("Created: %d.\n\r", new_task_id);
+
+  bwprintf("FirstUserTask: exiting\n\r");
+  Exit();
 }
