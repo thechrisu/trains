f22daf26b135772f427e3996c1b43c8dd40e4131
Thomas Broadley <buriedunderbooks@hotmail.com>
2018-01-24 23:05:10 -0500

    Swap meanings in test

--- usr/test/messaging/test_messaging_exit_with_blocked.c
+++ usr/test/messaging/test_messaging_exit_with_blocked.c
@@ -1,14 +1,14 @@
 #include "test_messaging.h"
 
-void send_blocked() {
+void receive_blocked() {
   Send(MyParentTid(), (char *)0, 0, (char *)0, 0);
-  bwprintf("Send-blocked: should never reach here\n\r");
+  bwprintf("Receive-blocked: should never reach here\n\r");
 }
 
-void receive_blocked() {
+void send_blocked() {
   int tid;
   Receive(&tid, (char *)0, 0);
-  bwprintf("Receive-blocked: should never reach here\n\r");
+  bwprintf("Send-blocked: should never reach here\n\r");
 }
 
 void reply_blocked() {
@@ -17,8 +17,8 @@ void reply_blocked() {
 }
 
 void test_messaging_exit_with_blocked() {
-  // receive_blocked will be blocked forever on a Receive call
-  Create(6, &receive_blocked);
+  // send_blocked will be blocked forever on a Receive call
+  Create(6, &send_blocked);
 
   // reply_blocked will be blocked forever waiting for a Reply call
   int tid;
@@ -27,10 +27,10 @@ void test_messaging_exit_with_blocked() {
   Assert(tid == reply_blocked_tid);
 
   /*
-    send_blocked will run directly after this call to Create. It calls Send immediately.
+    receive_blocked will run directly after this call to Create. It calls Send immediately.
     Since its parent (this task) still exists, it will block forever. If send_blocked
     had a lower priority than its parent, it would call Send after its parent had exited,
     causing it not to block.
   */
-  Create(6, &send_blocked);
+  Create(6, &receive_blocked);
 }
