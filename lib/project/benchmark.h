2c6c2f4229359663b812d69077a4250d5478395d
culshoefer <christophsulshoefer@gmail.com>
2018-01-25 01:44:46 -0500

    Added docs

--- lib/project/benchmark.h
+++ lib/project/benchmark.h
@@ -49,12 +49,32 @@ extern volatile int *tid_send;
 extern volatile int *tid_receive_reply;
 extern volatile int *is_receive;
 
+/**
+ * Given an array of messages (int), returns the average value.
+ * @param a Integer array (e.g. of measurement values)
+ * @return average of the NUM_MSG values.
+ */
 int16_t get_avg(int16_t a[NUM_MSG]);
 
+/**
+ * Returns the largest numer.
+ * @param a Run time (thus largest -> worst).
+ * @return The worst run time.
+ */
 int16_t get_worst(int16_t a[NUM_MSG]);
 
+/**
+ * Returns the index of the item in the given array with the largest value.
+ * @param a Array.
+ * @return Index in the input array with the largest value.
+ */
 int16_t get_worst_index(int16_t a[NUM_MSG]);
 
+/**
+ * Returns the variance of an array of measurement values.
+ * @param a Array of run times.
+ * @return Variance of the input array
+ */
 int16_t get_variance(int16_t a[NUM_MSG]);
 
 /**
@@ -65,7 +85,18 @@ int16_t get_variance(int16_t a[NUM_MSG]);
  */
 void zip_subtract(int16_t arg1[NUM_MSG], int16_t arg2[NUM_MSG], int16_t res[NUM_MSG]);
 
+/**
+ * Populates the measurement data structure with the given data.
+ * A measurement is defined to be an interval [data1, data2]
+ * @param m Measurement pointer.
+ * @param data1 Array of earlier measurement values.
+ * @param data2 Array of later measurement values.
+ */
 void process_measurement(struct measurement *m, int16_t data1[NUM_MSG], int16_t data2[NUM_MSG]);
 
+/**
+ * Prints an array of measurements.
+ * @param mms Array of size NUM_TIMEPOINTS - 2
+ */
 void print_measurement_array(struct measurement *mms);
 #endif /* TRAINS_BENCHMARK_H */
