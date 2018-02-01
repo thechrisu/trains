cfd35cc8ec0d93f29dc0e87eb960019a793a26b8
Christoph Ulshoefer <christophsulshoefer@gmail.com>
2018-01-31 23:24:35 -0500

    Added ready_queue_remove() tests

--- kernel/src/queue.h
+++ kernel/src/queue.h
@@ -84,6 +84,7 @@ void REMOVE(QUEUE_TYPE)(QUEUE_TYPE *q, task_descriptor *td) {
     } else {
       task_descriptor *prev = QUEUE_PREV(td);
       task_descriptor *next = QUEUE_NEXT(td);
+      *q = next;
       QUEUE_PREV(next) = prev;
       QUEUE_NEXT(prev) = next;
     }
