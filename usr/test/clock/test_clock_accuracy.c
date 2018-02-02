393c2cfa3e36f8e3dc818d7c1b2d98ac4a77db63
Thomas Broadley <buriedunderbooks@hotmail.com>
2018-02-02 15:07:39 -0500

    Extract method for testing delay accuracy

--- usr/test/clock/test_clock_accuracy.c
+++ usr/test/clock/test_clock_accuracy.c
@@ -1,23 +1,33 @@
 #include "test_clock.h"
 
-void clock_accuracy() {
-  message send, reply;
-  uint32_t start_time, elapsed_time;
+int clock_server_tid;
 
+void time_delay(uint32_t ticks) {
+  message send;
+  uint32_t start_time, elapsed_time;
+  char *s = ticks == 1 ? "" : "s";
+
+  send.type = MESSAGE_DELAY;
+  send.msg.message_delay_ticks = ticks;
+
+  bwprintf("Delaying %d tick%s\n\r", ticks, s);
+
+  start_time = get_time();
+  Assert(Send(clock_server_tid, &send, sizeof(send), EMPTY_MESSAGE, 0) == 0);
+  elapsed_time = get_time() - start_time;
+
+  bwprintf("Actual time for delaying %d tick%s: %d ms\n\r", ticks, s, elapsed_time);
+  Assert(elapsed_time >= 10 * (ticks - 1) && elapsed_time <= 10 * (ticks + 1));
+}
+
+void clock_accuracy() {
   setup_timer();
 
   ns_tid = Create(7, &nameserver_main);
-  int clock_server_tid = Create(5, &clock_server);
+  clock_server_tid = Create(5, &clock_server);
   Create(2, &idle_task);
 
-  send.type = MESSAGE_DELAY;
-  send.msg.message_delay_ticks = 10;
-
-  start_time = get_time();
-  Assert(Send(clock_server_tid, &send, sizeof(send), &reply, sizeof(reply)) == 0);
-  elapsed_time = get_time() - start_time;
-  bwprintf("Actual time for delaying 10 ticks: %d ms\n\r", elapsed_time);
-  Assert(elapsed_time >= 10 * (10 - 1) && elapsed_time <= 10 * (10 + 1));
+  time_delay(10);
 
   Assert(Kill(WhoIs("Idle")) == 0);
   Assert(Kill(WhoIs("ClockNotifier")) == 0);
