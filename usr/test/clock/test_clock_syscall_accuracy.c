b6d2c2e0f7489d01f8dee7206f52ded07918d90e
Thomas Broadley <buriedunderbooks@hotmail.com>
2018-02-03 17:41:58 -0500

    Update clock syscall accuracy test to match non-syscall version

--- usr/test/clock/test_clock_syscall_accuracy.c
+++ usr/test/clock/test_clock_syscall_accuracy.c
@@ -9,7 +9,7 @@ void time_syscall_delay(uint32_t ticks) {
   bwprintf("Delaying %d tick%s\n\r", ticks, s);
 
   start_time = get_time();
-  Delay(clock_server_tid, ticks);
+  Assert(Delay(clock_server_tid, ticks) == 0);
   elapsed_time = get_time() - start_time;
 
   bwprintf("Actual time for delaying %d tick%s: %d ms\n\r", ticks, s, elapsed_time);
@@ -24,9 +24,11 @@ void time_syscall_delay_until(uint32_t ticks) {
   bwprintf("Delaying until %d tick%s from now\n\r", ticks, s);
 
   start_time = get_time();
+
   ticks_at_start = Time(clock_server_tid);
   Assert(ticks_at_start > 0);
   Assert(DelayUntil(clock_server_tid, ticks_at_start + ticks) == 0);
+
   elapsed_time = get_time() - start_time;
 
   bwprintf("Actual time for delaying until %d tick%s from then: %d ms\n\r", ticks, s, elapsed_time);
@@ -42,18 +44,18 @@ void clock_syscall_accuracy() {
   clock_server_tid = Create(MyPriority() + 1, &clock_server);
   Create(2, &idle_task);
 
-  start_time = time();
   start_milliseconds = get_time();
+  start_time = Time(clock_server_tid);
 
   time_syscall_delay(1);
   time_syscall_delay(10);
   time_syscall_delay(50);
-  time_syscall_delay_until(1);
+  time_syscall_delay_until(5);
   time_syscall_delay_until(10);
   time_syscall_delay_until(50);
 
-  elapsed_time = time() - start_time;
-  elapsed_milliseconds = get_time() - elapsed_time;
+  elapsed_time = Time(clock_server_tid) - start_time;
+  elapsed_milliseconds = get_time() - start_milliseconds;
 
   bwprintf("Entire task took %d ticks (actual time %d milliseconds)\n\r", elapsed_time, elapsed_milliseconds);
   assert_within_n_ticks(elapsed_milliseconds, elapsed_time, 2);
