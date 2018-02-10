a31e267f9716699b9a173e1f9ccb8c25e3aa9ed3
Christoph Ulshoefer <christophsulshoefer@gmail.com>
2018-02-10 17:04:27 -0500

    Fixed test_rx_terminal (QEMU)

--- usr/test/awaitevent/test_rx_terminal.c
+++ usr/test/awaitevent/test_rx_terminal.c
@@ -8,16 +8,10 @@ void getter() {
 }
 
 void test_rx_terminal() {
-  ns_tid = Create(MyPriority() + 2, &nameserver_main);
-  Assert(Create(MyPriority() - 3, &idle_task) > 0);
   Assert(Create(MyPriority() - 1, &getter) > 0);
   for (int i = 0; i < 5; i++) {
     int a = AwaitEvent(TERMINAL_RX_INTERRUPT);
     // __asm__("MOV pc, #0x20000000\n\t");
     bwprintf("TerminalRxInterrupt return code: %d\n\r", a);
   }
-  int idle_tid = WhoIs("Idle");
-  Assert(idle_tid > 0);
-  Assert(Kill(idle_tid) == 0);
-  Assert(Kill(ns_tid) == 0);
 }
