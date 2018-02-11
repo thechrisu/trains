7ee7c7ea7a97275fa6a5139eaf19a837a5621383
Christoph Ulshoefer <christophsulshoefer@gmail.com>
2018-02-11 16:07:59 -0500

    Added some docs'

--- kernel/src/interrupt.h
+++ kernel/src/interrupt.h
@@ -42,5 +42,10 @@ extern int num_syscalls_total;
 
 #define NULL_TRAPFRAME (trapframe *)0
 
+/**
+ * Utility function to print a trapframe.
+ *
+ * @param tf The trapframe to print.
+ */
 void print_tf(trapframe *tf);
 #endif /* INTERRUPT_H */
