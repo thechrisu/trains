62297b6ca70c93b18624bc473435ed477d0674e7
Thomas Broadley <buriedunderbooks@hotmail.com>
2018-02-11 17:58:33 -0500

    Revert "Extract string formatting functions"
    This reverts commit e16b8730c5ed760e5829829f3aa05c0b735c9c9f.

--- include/io.h
+++ include/io.h
@@ -5,7 +5,20 @@
 #ifndef IO_H
 #define IO_H
 
-#include "format.h"
+#if !TESTING
+typedef char *va_list;
+
+#define __va_argsiz(t)                                          \
+  (((sizeof(t) + sizeof(int) - 1) / sizeof(int)) * sizeof(int))
+
+#define va_start(ap, pN) ((ap) = ((va_list) __builtin_next_arg(pN)))
+
+#define va_end(ap)    ((void)0)
+
+#define va_arg(ap, t)                                                   \
+  (((ap) = (ap) + __va_argsiz(t)), *((t*) (void*) ((ap) - __va_argsiz(t))))
+
+#endif /* TESTING */
 
 #define ON      1
 #define OFF     0
