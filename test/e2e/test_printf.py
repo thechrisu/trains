7cda6ebf62806db715f6f00e9ca68189076e5c26
Thomas Broadley <buriedunderbooks@hotmail.com>
2018-02-12 20:50:47 -0500

    Fix expected output in E2E test

--- test/e2e/test_printf.py
+++ test/e2e/test_printf.py
@@ -23,4 +23,4 @@ class TestPrintf(unittest.TestCase):
     def test_printf_happy_path(self):
         real_output = qemu_oneshot_test('printf_happy_path', '', TIMEOUT, timer_interrupts_on = True, iointerrupts_on = True)
         real_lines = list(filter(lambda x: x != '', real_output.split('\n\r')))
-        self.assertEqual(real_lines, expected_lines_printf_errors)
+        self.assertEqual(real_lines, expected_lines_printf_happy_path)
