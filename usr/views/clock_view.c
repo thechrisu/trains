6d5918083d41b26a1e046ceec21a172e29c45a97
Thomas Broadley <buriedunderbooks@hotmail.com>
2018-02-13 09:12:31 -0500

    Add K4 cursor-addressing clock view

---
+++ usr/views/clock_view.c
@@ -0,0 +1,20 @@
+#include "clock_view.h"
+
+void print_clock(int sender_tid, uint32_t deciseconds) {
+  uint32_t minutes = deciseconds / 10 / 60;
+  uint32_t seconds = (deciseconds / 10) % 60;
+  deciseconds = deciseconds % 10;
+  Printf(sender_tid, "\033[2;1H%d:%d.%d\033[K", minutes, seconds, deciseconds);
+}
+
+void clock_view() {
+  int clock_server_tid = WhoIs("ClockServer");
+  int sender_tid = WhoIs("TerminalTxServer");
+  uint32_t deciseconds = 0;
+
+  while (true) {
+    print_clock(sender_tid, deciseconds);
+    deciseconds += 1;
+    Delay(clock_server_tid, 10);
+  }
+}
---
+++ usr/views/clock_view.h
@@ -0,0 +1,13 @@
+/**
+ * @file
+ * @brief Clock accurate to 0.1 seconds, displayed using cursor addressing.
+ */
+#ifndef CLOCK_VIEW_H
+#define CLOCK_VIEW_H
+
+#include "codes.h"
+#include "tstdlib.h"
+
+void clock_view();
+
+#endif /* CLOCK_VIEW_H */
