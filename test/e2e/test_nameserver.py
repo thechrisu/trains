84a752f4f5e26b016700a5cf0a7c8c0ad069460b
Thomas Broadley <buriedunderbooks@hotmail.com>
2018-02-13 21:12:39 -0500

    Remove nameserver errors test

--- test/e2e/test_nameserver.py
+++ test/e2e/test_nameserver.py
@@ -2,15 +2,6 @@ import unittest
 
 from qemu_tcp_wrapper import qemu_oneshot_test
 
-expected_errorcodes_output = [
-    'Truncated: %d' % (ord('T') + 128),
-    'Too short(0): %d' % (ord('S') + 128),
-    'Too short(1): %d' % (ord('S') + 128),
-    'Too short(2): %d' % (ord('S') + 128),
-    'Wrong: %d' % (ord('W') + 128),
-    'Not found: %d' % (ord('N') + 128)
-]
-
 expected_wrapper_errors_output = [
     'Truncated RegisterAs: -2',
     'Truncated WhoIs: -2',
@@ -33,13 +24,6 @@ expected_happypath = [
 
 
 class TestNameserver(unittest.TestCase):
-    def test_errorcodes(self):
-        terminal_output = qemu_oneshot_test('test_nameserver_errors', '', 10)
-        lines = list(filter(lambda x: x != '', terminal_output.split('\n\r')))
-        self.assertEqual(len(lines), len(expected_errorcodes_output))
-        for i, exp in enumerate(expected_errorcodes_output):
-            self.assertEqual(lines[i], exp)
-
     def test_wrapper_errors(self):
         terminal_output = qemu_oneshot_test('test_nameserver_wrapper_errors', '', 10)
         lines = list(filter(lambda x: x != '', terminal_output.split('\n\r')))
