e59c3c6a0e630c0dc63e89c6c09aa578c5e6ef77
Thomas Broadley <buriedunderbooks@hotmail.com>
2018-02-15 14:18:16 -0500

    Switch one switch at a time

--- usr/train/switch_resetter.c
+++ usr/train/switch_resetter.c
@@ -1,7 +1,8 @@
 #include "switch_resetter.h"
 
-static int tx_server_tid;
+static int clock_server_tid;
 static int track_state_controller_tid;
+static int tx_server_tid;
 
 void switch_turnout(int switch_num, bool curved) {
   message send;
@@ -15,31 +16,21 @@ void switch_turnout(int switch_num, bool curved) {
   send.msg.turnout_switched_params.turnout_num = switch_num;
   send.msg.turnout_switched_params.state = curved ? TURNOUT_CURVED : TURNOUT_STRAIGHT;
   Assert(Send(track_state_controller_tid, &send, sizeof(send), EMPTY_MESSAGE, 0) >= 0);
-}
 
-void disable_solenoid() {
+  Delay(clock_server_tid, 15);
   Putc(tx_server_tid, TRAIN, (char)0x20);
 }
 
 void switch_resetter() {
-  int clock_server_tid = WhoIs("ClockServer");
+  clock_server_tid = WhoIs("ClockServer");
   tx_server_tid = WhoIs("TrainTxServer");
   track_state_controller_tid = WhoIs("TrackStateController");
 
-  for (int i = 1; i <= 17; i += 2) {
+  for (int i = 1; i <= 18; i += 1) {
     switch_turnout(i, true);
-    switch_turnout(i + 1, true);
-    Delay(clock_server_tid, 15);
-    disable_solenoid();
   }
-
   switch_turnout(153, true);
   switch_turnout(154, false);
-  Delay(clock_server_tid, 15);
-  disable_solenoid();
-
   switch_turnout(155, true);
   switch_turnout(156, false);
-  Delay(clock_server_tid, 15);
-  disable_solenoid();
 }
