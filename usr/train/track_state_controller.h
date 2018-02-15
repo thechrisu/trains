5ad4637013af2e69a47f56f021c3d07a592bb130
Christoph Ulshoefer <c@culshoefer.com>
2018-02-14 22:24:39 -0500

    Added track state controller

---
+++ usr/train/track_state_controller.h
@@ -0,0 +1,18 @@
+/**
+ * @file
+ * @brief Track State Controller.
+ */
+#ifndef TRACK_STATE_CONTROLLER
+#define TRACK_STATE_CONTROLLER
+
+#include "codes.h"
+#include "messages.h"
+#include "../track/track_data.h"
+#include "../track/track_node.h"
+
+/**
+ * Source of truth for the train track.
+ */
+void track_state_controller();
+
+#endif /* TRACK_STATE_CONTROLLER */
