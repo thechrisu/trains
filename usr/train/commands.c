7a37fd7d855a8a5f41d2689e8b0d9ece9dce64d6
Thomas Broadley <buriedunderbooks@hotmail.com>
2018-02-22 16:26:57 -0500

    Improve assertions

--- usr/train/commands.c
+++ usr/train/commands.c
@@ -10,33 +10,33 @@ void set_train_speed(int train_tx_server_tid, int track_state_controller_tid, in
   send.type = MESSAGE_TRAINSETSPEED;
   send.msg.train = train;
   send.msg.tr_data.should_speed = speed;
-  Assert(Send(track_state_controller_tid, &send, sizeof(send), EMPTY_MESSAGE, 0) >= 0);
+  Assert(Send(track_state_controller_tid, &send, sizeof(send), EMPTY_MESSAGE, 0) == 0);
 }
 
 void reverse_train(int train_tx_server_tid, int track_state_controller_tid, int train, bool current_direction) {
   char send_reverse_cmd[2];
   send_reverse_cmd[0] = REVERSE_SPEED;
   send_reverse_cmd[1] = train;
-  PutBytes(train_tx_server_tid, send_reverse_cmd, 2);
+  Assert(PutBytes(train_tx_server_tid, send_reverse_cmd, 2) == 0);
 
   message send;
   send.type = MESSAGE_TRAINREVERSED;
   send.msg.train = train;
   send.msg.tr_data.direction = !current_direction;
-  Assert(Send(track_state_controller_tid, &send, sizeof(send), EMPTY_MESSAGE, 0) >= 0);
+  Assert(Send(track_state_controller_tid, &send, sizeof(send), EMPTY_MESSAGE, 0) == 0);
 }
 
 void switch_turnout(int clock_server_tid, int train_tx_server_tid, int track_state_controller_tid, int turnout_num, bool curved) {
   char buf[2];
   buf[0] = (char)(curved ? 0x22 : 0x21);
   buf[1] = (char)turnout_num;
-  PutBytes(train_tx_server_tid, buf, 2);
+  Assert(PutBytes(train_tx_server_tid, buf, 2) == 0);
 
   message send;
   send.type = MESSAGE_TURNOUTSWITCHED;
   send.msg.turnout_switched_params.turnout_num = turnout_num;
   send.msg.turnout_switched_params.state = curved ? TURNOUT_CURVED : TURNOUT_STRAIGHT;
-  Assert(Send(track_state_controller_tid, &send, sizeof(send), EMPTY_MESSAGE, 0) >= 0);
+  Assert(Send(track_state_controller_tid, &send, sizeof(send), EMPTY_MESSAGE, 0) == 0);
 
   Delay(clock_server_tid, 15);
   Putc(train_tx_server_tid, TRAIN, (char)0x20);
