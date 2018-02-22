a9fa7ee193709df77fb73f5f26a9256879e4a46f
Thomas Broadley <buriedunderbooks@hotmail.com>
2018-02-22 16:07:23 -0500

    Make function name shorter

--- usr/train/commands.c
+++ usr/train/commands.c
@@ -6,11 +6,11 @@ void set_train_speed(int train_tx_server_tid, int track_state_controller_tid, in
   to_send_bytes[1] = train;
   Assert(PutBytes(train_tx_server_tid, to_send_bytes, 2) == 0);
 
-  message train_data_msg;
-  train_data_msg.type = MESSAGE_TRAINSETSPEED;
-  train_data_msg.msg.train = train;
-  train_data_msg.msg.tr_data.should_speed = speed;
-  Assert(Send(track_state_controller_tid, &train_data_msg, sizeof(train_data_msg), EMPTY_MESSAGE, 0) >= 0);
+  message send;
+  send.type = MESSAGE_TRAINSETSPEED;
+  send.msg.train = train;
+  send.msg.tr_data.should_speed = speed;
+  Assert(Send(track_state_controller_tid, &send, sizeof(send), EMPTY_MESSAGE, 0) >= 0);
 }
 
 void switch_turnout(int clock_server_tid, int train_tx_server_tid, int track_state_controller_tid, int turnout_num, bool curved) {
