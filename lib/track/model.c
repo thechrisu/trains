18918bac1faf584cf33f5daa7f1e20bcb995149f
Christoph Ulshoefer <c@culshoefer.com>
2018-02-23 16:46:54 -0500

    Added train speed->velocity map helper

---
+++ lib/track/model.c
@@ -0,0 +1,47 @@
+#include "model.h"
+
+default_speed default_speeds[] = {
+  { // Guard element
+    .train = 1337,
+    .speed = 1337,
+    .velocity = 1337
+  }
+};
+
+uint32_t super_default_speeds[15] = {
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0
+};
+
+void setup_speed_to_velocity_map(uint32_t map[81][15], default_speed defaults[], uint32_t super_defaults[15]) {
+  for (int i = 0; i < 81; i++) {
+    for (int j = 0; j < 15; i++) {
+      map[i][j] = super_defaults[j];
+    }
+  }
+
+  default_speed *current_speed = (default_speed *)defaults;
+  while (!(current_speed->train == 1337 && current_speed->speed == 1337 && current_speed->velocity == 1337)) {
+    int t = current_speed->train;
+    int s = current_speed->speed;
+    uint32_t v = current_speed->velocity;
+    Assert(t >= 1 && t <= 81);
+    Assert(s >= 0 && s <= 14);
+    Assert(v < DEFINITE_MAX_CM_PER_SEC * 10 * 100);
+    map[t][s] = v;
+    current_speed += 1;
+  }
+}
