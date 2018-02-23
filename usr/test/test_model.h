5d0dd0b21cf5dfb9b74930ca76ed18408c27aaa6
culshoefer <christophsulshoefer@gmail.com>
2018-02-23 18:12:57 -0500

    Added E2E test for constant velocity model

---
+++ usr/test/test_model.h
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * @brief Test functions for the train model.
+ */
+#ifndef TEST_MODEL_H
+#define TEST_MODEL_H
+
+#include "codes.h"
+#include "messages.h"
+#include "nameserver.h"
+#include "./nameserver/ns_tid.h"
+#include "../train/track_state_controller.h"
+
+void test_constant_velocity_model();
+
+#endif /* TEST_MODEL_H */
