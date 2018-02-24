ce91ba78977c22ed9b3e876cb0f38dbc32781023
Thomas Broadley <buriedunderbooks@hotmail.com>
2018-02-24 16:38:53 -0500

    Add documentation

--- usr/train/train_util.h
+++ usr/train/train_util.h
@@ -7,8 +7,19 @@
 
 void get_leading_edge(int16_t old_sensors[10], int16_t new_sensors[10], int16_t leading_edge[10]);
 
+/**
+ * @param   sensors A set of sensor data.
+ * @param   offset  The offset of a sensor within the sensor data bitmap.
+ * @returns Whether or not the sensor is triggered in the sensor data.
+ */
 bool is_sensor_triggered(int16_t sensors[10], unsigned int offset);
 
+/**
+ * Query the track state controller until the given sensor has been triggered.
+ * @param clock_server_tid
+ * @param track_state_controller_tid
+ * @param offset                     The offset of a sensor within the sensor data bitmap.
+ */
 void poll_until_sensor_triggered(int clock_server_tid, int track_state_controller_tid, unsigned int offset);
 
 #endif /* TRAIN_UTIL_H */
