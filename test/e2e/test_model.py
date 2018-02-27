617e3ced4076f7739ccb7f5e260d286fe0b17628
culshoefer <c@culshoefer.com>
2018-02-26 19:10:22 -0500

    Fix test/indentation

--- test/e2e/test_model.py
+++ test/e2e/test_model.py
@@ -8,11 +8,13 @@ class TestModel(unittest.TestCase):
         ret = qemu_oneshot_test('test_constant_velocity_model', '', TIMEOUT)
         self.assertEqual(ret, 'Success.\n\r')
 
+
     def test_stopping_distance_model(self):
         ret = qemu_oneshot_test('test_stopping_distance_model', '', TIMEOUT)
         self.assertEqual(ret, 'Success.\n\r')
 
-     def test_stopping_time_model(self):
+
+    def test_stopping_time_model(self):
         ret = qemu_oneshot_test('test_stopping_time_model', '', TIMEOUT)
         self.assertEqual(ret, 'Success.\n\r')
 
