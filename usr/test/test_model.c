76110520944f399d9170a13e4b522413e73d79a8
Thomas Broadley <buriedunderbooks@hotmail.com>
2018-02-27 16:11:56 -0500

    Test that calibration with zero time is ignored

--- usr/test/test_model.c
+++ usr/test/test_model.c
@@ -36,6 +36,12 @@ void test_constant_velocity_model() {
     Assert(receive.msg.train_speeds[i] == first_train_speeds[i]);
   }
   assert_valid_velocity(14, receive.msg.train_speeds[14]);
+  first_train_speeds[14] = receive.msg.train_speeds[14];
+
+  update_constant_velocity_model(track_state_controller_tid, 1, 14, sensor_offset('C', 15), sensor_offset('C', 15), 0);
+  for (int i = 0; i <= 14; i++) {
+    Assert(receive.msg.train_speeds[i] == first_train_speeds[i]);
+  }
 
   Assert(Kill(track_state_controller_tid) == 0);
 
