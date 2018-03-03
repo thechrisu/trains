d6d58b442150c65a134fee9895827080c5669913
culshoefer <c@culshoefer.com>
2018-03-02 20:01:46 -0500

    Add frame for stop d calib

--- usr/train/train_conductor.c
+++ usr/train/train_conductor.c
@@ -18,6 +18,59 @@ void conductor_reverse(int train_tx_server, int track_state_controller, int cloc
   d->time_speed_last_changed = Time(clock_server);
 }
 
+// Only performs one iteration of calibration
+void conductor_calib_sd_one_iter(int train_tx_server, int track_state_controller,
+                                 int clock_server, train_data *d, int speed) {
+  // TODO precompute distance from D9 to B10
+  set_train_speed_and_headlights(train_tx_server, track_state_controller,
+                                 d->train, 0, true);
+  unsigned int start_sensor, goal_sensor;
+  if (speed == 14) {
+    start_sensor = sensor_offset('D', 9);
+    goal_sensor = sensor_offset('B', 10);
+  } else if (speed <= 9) {
+    start_sensor = sensor_offset('D', 11);
+    goal_sensor = sensor_offset('C', 16);
+  } else if (speed >= 12) {
+    start_sensor = sensor_offset('E', 12);
+    goal_sensor = sensor_offset('B', 10);
+  } else {
+    start_sensor = sensor_offset('C', 8);
+    goal_sensor = sensor_offset('B', 10);
+  }
+  int d = 100 * distance_between_sensors_helper(find_sensor(start_sensor),
+                                                find_sensor(goal_sensor),
+                                                0, 15);
+  // sensor_offset('D' 9), sensor_offset('B', 10)
+  // go back twice stopping distance (once: until start sensor)
+  // then decelarate
+  // then reverse + accelerate to speed
+  // poll until we hit the start sensor D9
+  // compute (distance_to_b10 - estimated_stopping_distance)
+  // compute stopping time, as well
+  // then wait a while
+  // poll B10?
+  // if B10 hit, compute diff (time), new stopping distance = old * 0.9 + diff * c
+  // if no B10 hit, crawl forward at speed 1 until we hit it, record time.
+  // update stopping distance/time estimate
+}
+
+void conductor_calib_sd(int train_tx_server, int track_state_controller,
+                        int clock_server, train_data *d, int speed) {
+  switch_turnout(clock_server, train_tx_server, track_state_controller, 3, false);
+  switch_turnout(clock_server, train_tx_server, track_state_controller, 5, true);
+  switch_turnout(clock_server, train_tx_server, track_state_controller, 7, true);
+  switch_turnout(clock_server, train_tx_server, track_state_controller, 8, false);
+  switch_turnout(clock_server, train_tx_server, track_state_controller, 9, false);
+  switch_turnout(clock_server, train_tx_server, track_state_controller, 11, false);
+  switch_turnout(clock_server, train_tx_server, track_state_controller, 18, false);
+
+  for (int i = 0; i < 5; i++) {
+    conductor_calib_sd_one_iter(train_tx_server, track_state_controller,
+                                clock_server, d, speed);
+  }
+}
+
 void train_conductor() {
   int sender_tid;
   message received, ready;
