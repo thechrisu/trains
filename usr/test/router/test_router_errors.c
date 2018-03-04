2887c66211104364046755a8a4a63302ad8318ed
Thomas Broadley <buriedunderbooks@hotmail.com>
2018-03-04 18:46:16 -0500

    Improve router errors test

--- usr/test/router/test_router_errors.c
+++ usr/test/router/test_router_errors.c
@@ -7,15 +7,21 @@ void test_router_errors() {
   location end = { .sensor = sensor_offset('B', 12), .offset = 0 };
   reservation route[MAX_ROUTE_LENGTH];
 
+  Assert(cancel_route(71) == -1);
+  Assert(cancel_route(24) == -1);
   Assert(get_route(71, 10, &start, &end, route) == 0);
   Assert(get_route(71, 10, &start, &end, route) == -1);
   Assert(get_route(71, 10, &start, &end, route) == -1);
+  Assert(get_route(24, 10, &start, &end, route) == 0);
   Assert(cancel_route(71) == 0);
   Assert(cancel_route(71) == -1);
+  Assert(cancel_route(24) == 0);
   Assert(cancel_route(71) == -1);
+  Assert(cancel_route(24) == -1);
   Assert(get_route(71, 10, &start, &end, route) == 0);
   Assert(get_route(71, 10, &start, &end, route) == -1);
   Assert(cancel_route(71) == 0);
+  Assert(get_route(24, 10, &start, &end, route) == 0);
   Assert(cancel_route(71) == -1);
 
   router_test_teardown();
