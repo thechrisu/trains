fcfff79197be3e19116cb2bbf1bf6a20e5637af6
Thomas Broadley <buriedunderbooks@hotmail.com>
2018-03-04 18:33:04 -0500

    Add router sensor pair test to automated testing

--- test/e2e/test_router.py
+++ test/e2e/test_router.py
@@ -7,3 +7,11 @@ class TestRouter(unittest.TestCase):
     def test_router_basic(self):
         ret = qemu_oneshot_test('router_basic', '', TIMEOUT, timer_interrupts_on = True)
         self.assertEqual(ret, 'Success.\n\r')
+
+    def test_router_sensor_pair(self):
+        ret = qemu_oneshot_test('router_sensor_pair', '', TIMEOUT, timer_interrupts_on = True)
+        self.assertEqual(ret, 'Success.\n\r')
+
+    def test_router_errors(self):
+        ret = qemu_oneshot_test('router_errors', '', TIMEOUT, timer_interrupts_on = True)
+        self.assertEqual(ret, 'Success.\n\r')
