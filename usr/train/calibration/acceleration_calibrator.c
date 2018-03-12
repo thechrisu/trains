b61649a99a216510157a9bb17cdc29329cd68c93
culshoefer <c@culshoefer.com>
2018-03-12 17:57:53 -0400

    Add dummy acceleration calibration task

---
+++ usr/train/calibration/acceleration_calibrator.c
@@ -0,0 +1,9 @@
+#include "acceleration_calibrator.h"
+
+void dynamic_acceleration_calibrator() {
+  // TODO actually calibrate:
+  /*
+   Check if we are currently NOT moving at constant speed.
+   If so, be smart about the distance travelled since we last hit a sensor
+  */
+}
---
+++ usr/train/calibration/acceleration_calibrator.h
@@ -0,0 +1,6 @@
+#ifndef ACCELERATION_CALIBRATOR_H
+#define ACCELERATION_CALIBRATOR_H
+
+void dynamic_acceleration_calibrator();
+
+#endif /* ACCELERATION_CALIBRATOR_H */
