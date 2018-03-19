03a1a6889a1816a42d5e6e614b4e4f68d693951a
Thomas Broadley <buriedunderbooks@hotmail.com>
2018-03-19 19:20:19 -0400

    Attribute only if sensor is triggered

--- usr/train/sensors/sensor_interpreter.c
+++ usr/train/sensors/sensor_interpreter.c
@@ -149,17 +149,17 @@ void sensor_interpreter() {
                 break;
               }
             }
-          }
 
-          if (sensor_attributed_to == NOT_ATTRIBUTED) {
-            for (int i = 0; i < num_active_trains; i += 1) {
-              int train = active_trains[i];
-              int last_time = time_at_last_sensor_hit[train];
+            if (sensor_attributed_to == NOT_ATTRIBUTED) {
+              for (int i = 0; i < num_active_trains; i += 1) {
+                int train = active_trains[i];
+                int last_time = time_at_last_sensor_hit[train];
 
-              if (train_is_lost(tr_data[train].should_speed, current_time - last_time)) {
-                attribute_sensor(train, sensor, current_time);
-                sensor_attributed_to = train;
-                break;
+                if (train_is_lost(tr_data[train].should_speed, current_time - last_time)) {
+                  attribute_sensor(train, sensor, current_time);
+                  sensor_attributed_to = train;
+                  break;
+                }
               }
             }
           }
