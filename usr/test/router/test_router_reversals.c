1c61f55e0e14d204891f0ac11bd2945999daa190
culshoefer <c@culshoefer.com>
2018-03-20 15:10:03 -0400

    Add siding edge case

--- usr/test/router/test_router_reversals.c
+++ usr/test/router/test_router_reversals.c
@@ -1,5 +1,23 @@
 #include "test_router.h"
 
+void test_single_reversal_siding() {
+  // Note: If the first sensor is the pair of the last sensor/the direction
+  // we're going in, we treat that as a hint that we should reverse first,
+  // before starting to route
+  location start = { .sensor = sensor_offset('B', 10), .offset = 0 };
+  location end = { .sensor = sensor_offset('A', 10), .offset = 0 };
+  track_node *route[MAX_ROUTE_LENGTH];
+
+  Assert(get_route(&start, &end, route) == 0);
+
+  int length = route_node_count(route);
+  Assert(length == 8);
+  char *expected_segments[] = { "B10", "B9", "A5", "MR3", "BR3", "BR2", "BR1", "A9" };
+  for (int i = 0; i < length; i++) {
+    Assert(tstrcmp((char *)route[i]->name, expected_segments[i]));
+  }
+}
+
 void test_single_reversal() {
   location start = { .sensor = sensor_offset('C', 9), .offset = 0 };
   location end = { .sensor = sensor_offset('C', 5), .offset = 0 };
@@ -35,6 +53,7 @@ void test_double_reversal() {
 void test_router_reversals() {
   router_test_setup();
   test_single_reversal();
+  test_single_reversal_siding();
   test_double_reversal();
   bwprintf("Success.\n\r");
   router_test_teardown();
