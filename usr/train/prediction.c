06ea27cef0ff0009dcbee8152813fff18c5dd553
Thomas Broadley <buriedunderbooks@hotmail.com>
2018-03-24 17:06:50 -0400

    Fix references to *_sensor methods

--- usr/train/prediction.c
+++ usr/train/prediction.c
@@ -18,18 +18,18 @@ void predict_sensor_hit(int train_coordinates_server_tid,
     return;
   }
 
-  track_node *next_sensor = sensor_next(current.loc.node, turnout_states);
+  track_node *next_track_node = track_node_next(current.loc.node, turnout_states);
 
-  if (next_sensor == NULL_TRACK_NODE) {
+  if (next_track_node == NULL_TRACK_NODE) {
     prediction->loc.node = NULL_TRACK_NODE;
     prediction->ticks = INFINITE_TICKS;
     return;
   }
 
-  prediction->loc.node = next_sensor;
+  prediction->loc.node = next_track_node;
   prediction->loc.offset = 0;
-  int dist_to_next_sensor = 100 * distance_between_sensors(current.loc.node,
-                                                           next_sensor) - current.loc.offset;
+  int dist_to_next_track_node = 100 * distance_between_track_nodes(current.loc.node,
+                                                                   next_track_node) - current.loc.offset;
 
   if (current.velocity == 0) {
     prediction->ticks = INFINITE_TICKS;
@@ -38,23 +38,23 @@ void predict_sensor_hit(int train_coordinates_server_tid,
     int max_distance_before_stop = (int)((current_velocity * current_velocity) /
                                          (2 * -current.acceleration));
 
-    if (max_distance_before_stop < dist_to_next_sensor) {
+    if (max_distance_before_stop < dist_to_next_track_node) {
       prediction->ticks = INFINITE_TICKS;
     } else {
       long long to_sqrt = (current_velocity * current_velocity) +
-                          2 * current.acceleration * dist_to_next_sensor;
+                          2 * current.acceleration * dist_to_next_track_node;
       int sqrt_result = (int)(1.0F / tinvsqrt((float)to_sqrt));
       prediction->ticks = current.ticks + (100 * (sqrt_result - current_velocity)) /
                                           current.acceleration;
     }
   } else if (current.acceleration == 0) {
-    prediction->ticks = current.ticks + 100 * dist_to_next_sensor / current.velocity;
+    prediction->ticks = current.ticks + 100 * dist_to_next_track_node / current.velocity;
   } else {
     long long velocity_diff = current.velocity - current.target_velocity;
-    long long ticks_to_next_sensor = 100 * dist_to_next_sensor / current.target_velocity +
-                                     100 * (velocity_diff * velocity_diff) /
-                                     (2 * current.acceleration * current.target_velocity);
-    prediction->ticks = current.ticks + (int)ticks_to_next_sensor;
+    long long ticks_to_next_track_node = 100 * dist_to_next_track_node / current.target_velocity +
+                                         100 * (velocity_diff * velocity_diff) /
+                                         (2 * current.acceleration * current.target_velocity);
+    prediction->ticks = current.ticks + (int)ticks_to_next_track_node;
   }
 
 }
