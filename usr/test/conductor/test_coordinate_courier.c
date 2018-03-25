211ce1f33833c4ada71c6861546c5c6ed355ad95
culshoefer <c@culshoefer.com>
2018-03-25 14:29:32 -0400

    Add nontrivial coordinate_to_notification test cases

--- usr/test/conductor/test_coordinate_courier.c
+++ usr/test/conductor/test_coordinate_courier.c
@@ -34,7 +34,7 @@ void test_coord_courier_not_located_yet() {
   coord_courier_test_teardown(courier_tid);
 }
 
-void test_coordinates_to_notification() {
+void test_coordinates_to_notification_basic() {
   track_state t;
   coordinates current, last;
   location_notification locations_to_observe[MAX_LOCATIONS_TO_OBSERVE];
@@ -50,8 +50,56 @@ void test_coordinates_to_notification() {
   Assert(new.reason == GOT_LOST);
 }
 
+void test_coordinates_to_notification_complex() {
+  track_state t;
+  init_track(&t);
+  coordinates current, last;
+  location_notification locations_to_observe[MAX_LOCATIONS_TO_OBSERVE];
+  bool is_location_set[MAX_LOCATIONS_TO_OBSERVE];
+  location_notification new;
+
+  locations_to_observe[0].loc.node = &t.tracka[0];
+  current.loc.node = &t.tracka[0];
+  last.loc.node = NULL_TRACK_NODE;
+
+  Assert(coordinates_to_notification(&current, &last, locations_to_observe, is_location_set, &new));
+  Assert(new.reason == LOCATION_CHANGED);
+  Assert(coordinates_to_notification(&current, &last, locations_to_observe, is_location_set, &new));
+  Assert(new.reason == LOCATION_CHANGED);
+  Assert(coordinates_to_notification(&current, &last, locations_to_observe, is_location_set, &new));
+  Assert(new.reason == LOCATION_CHANGED);
+  Assert(new.loc.node == &t.tracka[0]);
+
+  is_location_set[0] = true;
+  locations_to_observe[0].reason = LOCATION_TO_STOP;
+  Assert(coordinates_to_notification(&current, &last, locations_to_observe, is_location_set, &new));
+  Assert(new.reason == LOCATION_TO_STOP);
+
+  last.loc.node = current.loc.node;
+  Assert(!coordinates_to_notification(&current, &last, locations_to_observe, is_location_set, &new));
+  Assert(!coordinates_to_notification(&current, &last, locations_to_observe, is_location_set, &new));
+  Assert(new.loc.node == &t.tracka[0]);
+
+  current.loc.node = find_node_by_name(&t, "BR15");
+  locations_to_observe[0].loc.node = current.loc.node;
+  locations_to_observe[0].loc.offset = current.loc.offset;
+  last.loc.node = NULL_TRACK_NODE;
+  locations_to_observe[0].reason = LOCATION_TO_SWITCH;
+  locations_to_observe[0].switch_to_switch[0] = 187; // Should not matter
+  locations_to_observe[0].switch_to_switch[1] = 186; // Should not matter
+  is_location_set[0] = true;
+  Assert(coordinates_to_notification(&current, &last, locations_to_observe, is_location_set, &new));
+  Assert(new.reason == LOCATION_TO_SWITCH);
+  Assert(new.switch_to_switch[0] == 187);
+  Assert(new.switch_to_switch[1] == 186);
+  Assert(new.loc.node == find_node_by_name(&t, "BR15"));
+  last.loc.node = current.loc.node;
+  Assert(!coordinates_to_notification(&current, &last, locations_to_observe, is_location_set, &new));
+}
+
 void test_coord_courier() {
   test_coord_courier_not_located_yet();
-  test_coordinates_to_notification();
+  test_coordinates_to_notification_basic();
+  test_coordinates_to_notification_complex();
   bwprintf("Success.\n\r");
 }
