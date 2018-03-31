885b994a6b7db07d7dc56668d8d80993a0860b21
culshoefer <c@culshoefer.com>
2018-03-31 15:09:50 -0400

    Re-add groups.h

---
+++ lib/groups.h
@@ -0,0 +1,33 @@
+/**
+ * @file
+ * @brief Train groups.
+ */
+#ifndef TRAIN_GROUPS_H
+#define TRAIN_GROUPS_H
+
+#define MAX_GROUP_MEMBERS 6
+#define MAX_GROUP_NAME_LEN 4
+#define MAX_GROUPS 6
+
+/**
+ * The idea is to address train groups via their name.
+ * This way, we can just keep the groups contiguously
+ * in the first <code>num_groups</code> elements of the tr_groups array,
+ * as opposed to having to use a bool-map if we used the array index as
+ * the group identifier.
+ */
+typedef struct {
+  int members[MAX_GROUP_MEMBERS];
+  int num_members;
+} train_group;
+
+typedef struct {
+  train_group g;
+  char group_name[MAX_GROUP_NAME_LEN];
+  int tid;
+} train_group_info;
+
+train_group_info tr_groups[6];
+int num_groups;
+
+#endif /* TRAIN_GROUPS_H */
