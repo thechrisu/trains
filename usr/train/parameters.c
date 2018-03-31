8c76a8943c4503674d4f9d4b1d9ea53e0717e439
culshoefer <c@culshoefer.com>
2018-03-31 16:00:05 -0400

    Use all four trains (setting them dynamically somehow crashes things)

--- usr/train/parameters.c
+++ usr/train/parameters.c
@@ -1,6 +1,6 @@
 #include "parameters.h"
 
-#define NUM_ACTIVE_TRAINS_DEFAULT 3
+#define NUM_ACTIVE_TRAINS_DEFAULT 4
 #define SWITCH_PADDING_DEFAULT    50
 
 int active_trains[81];
@@ -15,12 +15,15 @@ static char parameter_names[MAX_PARAMETER][PARAMETER_NAME_LENGTH];
 void init_parameters() {
   active_trains[0] = 24;
   active_trains[1] = 58;
-  active_trains[2] = 78;
+  active_trains[2] = 74;
+  active_trains[3] = 78;
 
   for (int i = 0; i < 81; i += 1) {
     expected_next_sensors[i] = NO_NEXT_SENSOR_EXPECTED;
   }
   expected_next_sensors[24] = sensor_offset('A', 5);
+  expected_next_sensors[58] = sensor_offset('A', 8);
+  expected_next_sensors[74] = sensor_offset('A', 8);
   expected_next_sensors[78] = sensor_offset('A', 8);
 
   num_active_trains = NUM_ACTIVE_TRAINS_DEFAULT;
