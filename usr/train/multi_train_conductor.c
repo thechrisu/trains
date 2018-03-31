825568a25756b38664c83f7f22f71f25af1772bb
culshoefer <c@culshoefer.com>
2018-03-31 13:55:37 -0400

    Add empty multitrainconductor task

---
+++ usr/train/multi_train_conductor.c
@@ -0,0 +1,84 @@
+#include "multi_train_conductor.h"
+
+#define CONDUCTOR_STOP_CHECK_INTERVAL 2
+#define CONDUCTOR_SENSOR_CHECK_INTERVAL 1
+
+#define SWITCH_LOOKAHEAD_NODES 10
+
+#define SWITCH_DIST 25000
+
+#define ABS(a) ((a) > 0 ? (a) : (-(a)))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
+void multi_conductor_setspeed(int train_tx_server, int track_state_controller,
+                        int train, int speed) {
+  set_train_speed(train_tx_server, track_state_controller,
+                  train, speed);
+  // TODO implement this for real
+}
+
+void multi_conductor_reverse_to_speed(int train_tx_server,
+                                int track_state_controller, int clock_server,
+                                int train, int speed) {
+  stop_and_reverse_train_to_speed(clock_server, train_tx_server,
+                                  track_state_controller, train, speed);
+  // TODO implement this for real
+}
+
+void multi_train_conductor() {
+  int sender_tid;
+  message received, ready;
+  int train_tx_server = WhoIs("TrainTxServer");
+  int terminal_tx_server = WhoIs("TerminalTxServer");
+  int clock_server = WhoIs("ClockServer");
+  int track_state_controller = WhoIs("TrackStateController");
+  int cmd_dispatcher = WhoIs("CommandDispatcher");
+  int train_coordinates_server = WhoIs("TrainCoordinatesServer");
+  Assert(train_tx_server > 0);
+  Assert(clock_server > 0);
+  Assert(track_state_controller > 0);
+  Assert(cmd_dispatcher > 0);
+  Assert(train_coordinates_server > 0);
+
+  train_data d;
+  d.last_speed = 0;
+  d.should_speed = 0;
+  d.time_speed_last_changed = Time(clock_server);
+
+  Assert(Receive(&sender_tid, &received, sizeof(received)) >= 0);
+  Assert(Reply(sender_tid, EMPTY_MESSAGE, 0) >= 0);
+  Assert(received.msg.train > 0 && received.msg.train <= 80); // TODO receive group
+  Assert(received.type == MESSAGE_CONDUCTOR_SETTRAIN);
+  d.train = received.msg.train;
+
+  ready.msg.train = d.train;
+  ready.type = MESSAGE_READY;
+  while (true) {
+    Assert(Receive(&sender_tid, &received, sizeof(received)) >= 0);
+    Assert(Reply(sender_tid, EMPTY_MESSAGE, 0) >= 0);
+    switch (received.type) {
+      case MESSAGE_USER:
+        switch (received.msg.cmd.type) {
+          case USER_CMD_LOOP:
+            // TODO
+            break;
+          case USER_CMD_TR:
+            break;
+          case USER_CMD_RV:
+            break;
+          case USER_CMD_R:
+            break; // TODO
+          default:
+            logprintf("Got user cmd message of type %d\n\r", received.msg.cmd.type);
+            Assert(0);
+            break;
+        }
+        Assert(Send(cmd_dispatcher, &ready, sizeof(ready), EMPTY_MESSAGE, 0) == 0);
+        break;
+      default:
+        logprintf("Got user cmd message of type %d\n\r", received.type);
+        Assert(0);
+        break;
+    }
+  }
+}
