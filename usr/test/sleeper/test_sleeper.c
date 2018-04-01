bd3c84a22d9952d1a0219bb585c953219a06c6bd
culshoefer <c@culshoefer.com>
2018-04-01 17:02:55 -0400

    Fix syntax

--- usr/test/sleeper/test_sleeper.c
+++ usr/test/sleeper/test_sleeper.c
@@ -4,8 +4,8 @@ void test_sleeper() {
   set_alarm(1);
   int sleeper_tid = -1;
   message empty;
-  Assert(Receive(&sleeper_tid, &message, sizeof(message)) == 0);
-  Assert(message.type == MESSAGE_WAKEUP);
+  Assert(Receive(&sleeper_tid, &empty, sizeof(empty)) == 0);
+  Assert(empty.type == MESSAGE_WAKEUP);
   Assert(Reply(sleeper_tid, EMPTY_MESSAGE, 0) == 0);
   bwprintf("Success.\n\r");
 }
