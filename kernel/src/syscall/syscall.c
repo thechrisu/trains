3eb593c4e8fb39488cdd81ac4d7ca8347f49aeb2
culshoefer <c@culshoefer.com>
2018-04-06 11:39:42 -0400

    Fix segfault test

--- kernel/src/syscall/syscall.c
+++ kernel/src/syscall/syscall.c
@@ -14,7 +14,9 @@ int syscall_create(int priority, void (*code)()) {
   logprintf("Got task descriptor memory\n\r");
 #endif /* CONTEXT_SWITCH_DEBUG */
   task_init(ret, priority, code, get_current_task());
+#ifndef E2ETESTING
   logprintf("Task %d is at %x\n\r", ret->tid, (unsigned int)code);
+#endif /* E2ETESTING */
 #if CONTEXT_SWITCH_DEBUG
   logprintf("Set up task in syscall_create, tf: %x\n\r", ret->tf);
 #endif /* CONTEXT_SWITCH_DEBUG */
