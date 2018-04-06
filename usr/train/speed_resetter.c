3d0fe669e93e76a0bbdaa9363d73c5a38c4cc8d0
Thomas Broadley <buriedunderbooks@hotmail.com>
2018-04-06 08:53:31 -0400

    Rewrite speed resetter

--- usr/train/speed_resetter.c
+++ usr/train/speed_resetter.c
@@ -1,17 +1,12 @@
 #include "speed_resetter.h"
 
-static int tx_server_tid;
-static int track_state_controller_tid;
-
-static void set_speed_with_tids(int train, int speed) {
-  set_train_speed(tx_server_tid, track_state_controller_tid, train, speed);
-}
-
 void speed_resetter() {
-  tx_server_tid = WhoIs("TrainTxServer");
-  track_state_controller_tid = WhoIs("TrackStateController");
+  int tx_server_tid = WhoIs("TrainTxServer");
+  int track_state_controller_tid = WhoIs("TrackStateController");
 
-  for (int i = 0; i <= num_active_trains; i += 1) {
-    set_speed_with_tids(active_trains[i], 0);
+  for (int i = 0; i < num_active_trains; i += 1) {
+    logprintf("%d\n\r", active_trains[i]);
+    set_train_speed(tx_server_tid, track_state_controller_tid,
+                    active_trains[i], 0);
   }
 }
