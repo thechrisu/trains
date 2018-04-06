3f793d9141989bbed007671fc0f6e65c9758c0fe
culshoefer <c@culshoefer.com>
2018-04-05 20:25:15 -0400

    Add antifreeze

---
+++ usr/anti_freezer.h
@@ -0,0 +1,13 @@
+/**
+ * @file
+ * @brief The sniper among the tasks.
+ */
+#ifndef TRAIN_ANTI_FREEZE_H
+#define TRAIN_ANTI_FREEZE_H
+
+#include "codes.h"
+#include "usage_stats.h"
+
+void anti_freezer();
+
+#endif /* TRAIN_ANTI_FREEZE_H */
