bb60a274ca6f8f9d28d190345e78e824e55546f8
Thomas Broadley <buriedunderbooks@hotmail.com>
2018-04-06 07:00:11 -0400

    Clean up antifreezer

--- usr/anti_freezer.c
+++ usr/anti_freezer.c
@@ -3,21 +3,15 @@
 void anti_freezer() {
   int clock_server = WhoIs("ClockServer");
   Assert(clock_server > 0);
+
   Delay(clock_server, 200); // To allow the other tasks to run
+
   int idle = WhoIs("Idle");
   Assert(idle > 0);
 
-
   usage_stats last_second;
   while (true) {
-    bool got_small_value = false;
     Delay(clock_server, 200);
-
-    LastSecondsProcUsage(&last_second);
-
-    got_small_value = last_second.ms_run[idle % MAX_TASKS] < 1;
-
-   Delay(clock_server, 200);
 
     LastSecondsProcUsage(&last_second);
     if (last_second.ms_run[idle % MAX_TASKS] < 1) {
